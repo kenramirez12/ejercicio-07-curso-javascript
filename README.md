0c79481ddac441ecff8643f46cc733c56e1e14c3
Ken Ramirez <kenramirezp@gmail.com>
2022-06-06 18:51:55 -0500

    Mi primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ejercicio - Clase 07
+
+## Alcance
+1. Formulario para insertar información de alumnos.
+2. Visualizar información de alumnos en una tabla de HTML.
---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * 1. Formulario para insertar información de alumnos.
+ *  - Datos de entrada: Nombre, Edad, Curso
+ *  - Cuando se envía el formulario:
+ *    - Capturar el valor de los datos del formulario
+ *    - Guardamos datos en un array
+ *    - Limpiamos el formulario
+ * 2. Visualizar información de alumnos en una tabla de HTML.
+ *  - Capturar el elemento HTML de la tabla (tbody)
+ *  - Crear una fila de tabla con la información capturada
+ *  - Insertar la fila creada en el elemento HTML de la tabla (tbody)
+ */
